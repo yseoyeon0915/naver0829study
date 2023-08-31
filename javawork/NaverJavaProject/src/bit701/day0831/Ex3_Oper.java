05f0dfd10c2c8c6e3a0709ed4b169e1c772f8725
yseoyeon0915 <yseoyeon0915@gmail.com>
2023-08-31 11:53:56 +0900

    bit701.day0831

--- javawork/NaverJavaProject/src/bit701/day0831/Ex3_Oper.java
+++ javawork/NaverJavaProject/src/bit701/day0831/Ex3_Oper.java
@@ -9,6 +9,14 @@ public class Ex3_Oper {
         Scanner sc=new Scanner (System.in);
         //점수를 입력받아서 1-100 점이 아닐경우는 일단 프로그램 종료
         //90 이상은 합격, 80 이상은 "재시험", 그 나머지는 "불합격"
+        System.out.println("점수를 입력하세요");
+        int score=sc.nextInt();
+        if(score<1 || score>100)
+        {
+        	System.out.println("잘못된 값 입력으로 종료합니다!!");
+        	return;//return은 원래 해당 메서드만 종료하는 명령어인데 main 메서드 종료는 곧 프로그램종료를 의미
+        }
+        System.out.println("입력된 점수:"+score+"점");
 	}
 
 }
