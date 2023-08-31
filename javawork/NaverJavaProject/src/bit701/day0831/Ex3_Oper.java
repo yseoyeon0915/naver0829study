1c7600d5b8a8e7fdb89ae7d5eac59860867635c7
yseoyeon0915 <yseoyeon0915@gmail.com>
2023-08-31 11:38:27 +0900

    bit701.day0831

---
+++ javawork/NaverJavaProject/src/bit701/day0831/Ex3_Oper.java
@@ -0,0 +1,14 @@
+package bit701.day0831;
+
+import java.util.Scanner;
+
+public class Ex3_Oper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc=new Scanner (System.in);
+        //점수를 입력받아서 1-100 점이 아닐경우는 일단 프로그램 종료
+        //90 이상은 합격, 80 이상은 "재시험", 그 나머지는 "불합격"
+	}
+
+}
