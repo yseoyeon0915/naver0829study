b4d05ba1e457a2805e471829fe5ae060711346d0
yseoyeon0915 <yseoyeon0915@gmail.com>
2023-09-08 19:11:22 +0900

    Object

---
+++ javawork/NaverJavaProject/src/bit701/day0908/hello/MyHello.java
@@ -0,0 +1,11 @@
+package bit701.day0908.hello;
+
+//다른 패
+public class MyHello {
+     public void hello()
+     {
+    	 System.out.println("다른 패키지의 hello 메서드");
+     }
+	
+
+}
---
+++ javawork/NaverJavaProject/src/bit701/day0908/test.java
@@ -0,0 +1,11 @@
+package bit701.day0908;
+
+public class test {
+
+   //인스턴스 멤버 변수
+	public String str1="Hello World!";
+	protected String str2="야호. 금요일이다!";
+	String str3="클래스를 공부합시다!";
+	private String str4="나만 볼거야!";
+
+}
