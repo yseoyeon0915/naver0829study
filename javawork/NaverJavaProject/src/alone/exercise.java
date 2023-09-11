b5bc5cc7762a9ee9ba2cfcd7802b2e12df11f5e6
yseoyeon0915 <yseoyeon0915@gmail.com>
2023-09-11 11:29:27 +0900

    constructor

---
+++ javawork/NaverJavaProject/src/alone/exercise.java
@@ -0,0 +1,22 @@
+package alone;
+
+public class exercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String id = args[0];
+	        String password = args[1];
+	        if (id.equals("egoing")) {
+	            if (password.equals("111111")) {
+	                System.out.println("right");
+	            } else {
+	                System.out.println("wrong");
+	            }
+	 
+	        } else {
+	            System.out.println("wrong");
+	        }
+	    }
+	
+	}
+	
