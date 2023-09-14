6a9745ce06a4b19509933df639fe05fe63642ae4
yseoyeon0915 <yseoyeon0915@gmail.com>
2023-09-14 16:57:24 +0900

    컬렉션

---
+++ javawork/NaverJavaProject/src/bit701/day0914/Ex8_Set.java
@@ -0,0 +1,30 @@
+package bit701.day0914;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Ex8_Set {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //Set<String> set=new HashSet<String>();
+		Set<String> set=new TreeSet<String>(); //순서대로 나옴
+        set.add("red");
+        set.add("blue");
+        set.add("red");
+        set.add("green");
+        set.add("green");
+        set.add("red");
+        set.add("blue");
+        set.add("red");
+        set.add("pink");
+        set.add("yellow");
+        System.out.println(set.size()); //중복된거 뺴고 5개
+        
+        for(String s:set)
+        	System.out.println(s);
+        
+	}
+
+}
