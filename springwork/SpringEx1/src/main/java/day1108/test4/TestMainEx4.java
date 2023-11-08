a888d21f357e07d4a5341fe6575cd719c90899fb
yseoyeon0915 <yseoyeon0915@gmail.com>
2023-11-08 21:11:25 +0900

    spring

---
+++ springwork/SpringEx1/src/main/java/day1108/test4/TestMainEx4.java
@@ -0,0 +1,18 @@
+package day1108.test4;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestMainEx4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        ApplicationContext context=new ClassPathXmlApplicationContext("appcontext1.xml");
+
+	      Bitcamp bit=(Bitcamp)context.getBean("bit");
+	      bit.infoProcess();
+	   }
+
+	}
+
+
